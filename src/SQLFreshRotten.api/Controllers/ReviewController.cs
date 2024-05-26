7b2b0c4450d9eabd61836855687b538232d684ed
Eber Guerra <eb1993_jar@hotmail.com>
2024-05-26 15:47:20 -0600

    bug, rebuild: Se ajusta estructura y ser corrige error en el en la lectura del archivo que carga los usuarios

---
+++ src/SQLFreshRotten.api/Controllers/ReviewController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SQLFreshRotten.api.LogicModels.Api;
+using SQLFreshRotten.api.LogicProcess.db;
+using SQLFreshRotten.api.LogicProcess.microservices;
+using SQLFreshRotten.api.Models;
+
+namespace SQLFreshRotten.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReviewController : Controller
+    {
+        [HttpPost("review-type/")]
+        public async Task<ActionResult<ResponseRequest<ResponseRewiew>>> ReviewType([FromBody] CriticRequest paramerts)
+        {
+            try
+            {
+                ReviewService service = new();
+                ResponseRewiew criticType = await service.GetReview(paramerts);
+                return Ok(new ResponseRequest<dynamic>()
+                {
+                    Data = new { Result = criticType },
+                    Description = "Tipo de review, obtenida",
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseRequest<dynamic>()
+                {
+                    Data = new ResponseRewiew(),
+                    Description = "Tipo de review, no obtenida",
+                    FailReponse = new FailResponse()
+                    {
+                        Exception = ex.Message,
+                        IsFail = true
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ src/SQLFreshRotten.api/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SQLFreshRotten.api.LogicModels.Api;
+using SQLFreshRotten.api.LogicProcess.db;
+using SQLFreshRotten.api.ProviderContext;
+
+namespace SQLFreshRotten.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly DbCtx _context;
+
+        public UserController (DbCtx context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet("validate-user/")]
+        public ActionResult<ResponseRequest<dynamic>>  ValidateUser([FromQuery] string user, [FromQuery] string password)
+        {
+            try
+            {
+                DbUser dbUser = new(_context);
+                bool hasAcces = dbUser.AuthenticateUser(user, password);
+                if (!hasAcces)
+                    throw new Exception("Contraseña o Usuario, incorrecto");
+
+                return Ok(new ResponseRequest<dynamic>()
+                {
+                    Data = new { HasAccess = hasAcces },
+                    Description = "Usuario, authenticado",
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseRequest<dynamic>()
+                {
+                    Data = new { HasAccess = false },
+                    Description = "Usuario, no authenticado",
+                    FailReponse = new FailResponse()
+                    {
+                        IsFail = true,
+                        Exception = ex.Message
+                    }
+                });
+            }
+        }
+    }
+}
